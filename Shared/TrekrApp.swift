12c60860f81366b7669a502a15a89712080e9acb
Raí Vieira Adriano <raivieiraadriano92@gmail.com>
2021-07-07 05:32:51 -0300

    Initial Commit

---
+++ Shared/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Shared
+//
+//  Created by Raí on 07/07/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
